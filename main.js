ee30e68bd6263a2137d295a9496c16acf9980a19
Igor Arthur <86696913+iguuh@users.noreply.github.com>
2023-07-29 23:50:44 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,68 @@
+//1-
+// alert('Hello World!')
+
+//2-
+//let numberOne = Number(prompt("digite o primeiro numero:"));
+//let numberTwo = Number(prompt("digite o segundo numero:"));
+//sum = (numberOne + numberTwo)
+//alert(sum)
+
+//3-
+//itIsaNumber = typeof 1;
+//let numberType = typeof 6;
+//if (itIsaNumber == numberType) {
+//   alert("É um número 1" )
+//} else {
+//    alert("Não é um número " )
+//}
+
+//4-
+//itsString = typeof s
+//string = typeof string
+//if (itsString == string) {
+//    alert("É uma string" )
+//}else{
+//    alert("Não é uma string" )
+//}
+
+//5-
+//itsBoolean = typeof true
+//boolean = typeof false
+//if (itsBoolean == boolean) {
+//    alert("É um boolean")
+//} else {
+//    alert("Não é um boolean")
+//}
+
+//6-
+//let numberOne = Number(prompt("Digite o primeiro número: "))
+//let numberTwo = Number(prompt("Digite o segundo número: "))
+
+//sub = (numberOne - numberTwo)
+//alert("A subtração é: " + sub)
+
+//7-
+//plus = (numberOne * numberTwo)
+//alert("A Multiplicação é: " + plus)
+
+//8- 
+//div = (numberOne / numberTwo)
+//alert("A divisão é: " + div)
+
+//9-
+
+//let numberOne = Number(prompt("Digite o número: "))
+//let total = numberOne / 2  
+//if ( total % 2 == 0){
+//    alert('é par ' + total)
+//} else {
+//    alert('não é par ' + total)
+//}
+
+//10
+//let number = Number(prompt("Digite o número: "))
+//if ( number % 2 === 1){
+//    alert('é impar ' + number)
+//} else {
+//   alert('não é impar ' + number)
+//}
